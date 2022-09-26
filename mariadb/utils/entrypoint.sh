473df0792e9efbdfe37232e31ef23f6874e67259
AntoninLiens <antoninliens@hotmail.be>
2022-09-26 17:47:29 +0200

    test

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/26 17:44:18 by aliens            #+#    #+#              #
-#    Updated: 2022/09/26 17:45:31 by aliens           ###   ########.fr        #
+#    Updated: 2022/09/26 17:47:27 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -35,10 +35,12 @@ mysql -e "DROP USER ''@'$(hostname)'"
 echo "kill off the demo database"
 mysql -e "DROP DATABASE test"
 
-# echo "create $MARIADB_DATABASE"
-# mysql -e "create database $MARIADB_DATABASE"
-# mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
-# mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
+echo "create $MARIADB_DATABASE"
+mysql -e "create database $MARIADB_DATABASE"
+mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
+mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
 
 echo "make our changes take effect"
 mysql -e "FLUSH PRIVILEGES"
+
+exec /usr/bin/mysqld --user=mysql --console
