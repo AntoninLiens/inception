4e64e64db81b90228fa92a94b4fd8b186699a8f0
AntoninLiens <antoninliens@hotmail.be>
2022-09-26 17:32:54 +0200

    test

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo update root password
+echo "update root password"
 mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
 
-echo kill the anonymous users
+echo "kill the anonymous users"
 mysql -e "DROP USER ''@'localhost'"
 mysql -e "DROP USER ''@'$(hostname)'"
 
-echo kill off the demo database
+echo "kill off the demo database"
 mysql -e "DROP DATABASE test"
 
-echo create $MARIADB_DATABASE
+echo "create $MARIADB_DATABASE"
 mysql -e "create database $MARIADB_DATABASE"
 mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
 mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
 
-echo make our changes take effect
+echo "make our changes take effect"
 mysql -e "FLUSH PRIVILEGES"
