8f595942275799ee0b47ea74dee1cff6d25bfacd
AntoninLiens <antoninliens@hotmail.be>
2022-09-26 17:31:00 +0200

    test

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+echo update root password
 mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
-# Kill the anonymous users
+
+echo kill the anonymous users
 mysql -e "DROP USER ''@'localhost'"
-# Because our hostname varies we'll use some Bash magic here.
 mysql -e "DROP USER ''@'$(hostname)'"
-# Kill off the demo database
+
+echo kill off the demo database
 mysql -e "DROP DATABASE test"
 
+echo create $MARIADB_DATABASE
 mysql -e "create database $MARIADB_DATABASE"
 mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
 mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
 
-# Make our changes take effect
+echo make our changes take effect
 mysql -e "FLUSH PRIVILEGES"
