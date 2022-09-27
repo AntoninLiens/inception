e6b37ef8aa94796afb11ab17a2f93dc7477098a0
Antoninliens <antoninliens@hotmail.be>
2022-09-28 00:59:22 +0200

    test

--- mariadb/utils/entrypoint.sh
+++ mariadb/utils/entrypoint.sh
@@ -6,34 +6,23 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/26 17:44:18 by aliens            #+#    #+#              #
-#    Updated: 2022/09/27 18:04:07 by aliens           ###   ########.fr        #
+#    Updated: 2022/09/28 00:59:10 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/sh
+if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
+	service mysql start
 
-# # Give owner and group for the the DB which is normally automatically created in this folder 
-# chown -R mysql:mysql /var/lib/mysql
-# # Create a folder for the daemon (mysql server’s socket file)
-# mkdir -p /var/run/mysqld
-# #Give owner and group to that too
-# chown -R mysql:mysql /var/run/mysqld
-# #touch /var/run/mysqld/mysqlf.pid
-# mkfifo /var/run/mysqld/mysqld.sock
+	echo "update root password"
+	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
 
-service mysql start
+	echo "create $MARIADB_DATABASE"
+	mysql -e "create database $MARIADB_DATABASE"
+	mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
+	mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
 
-echo "update root password"
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
+	echo "make our changes take effect"
+	mysql -e "FLUSH PRIVILEGES"
 
-echo "create $MARIADB_DATABASE"
-mysql -e "create database $MARIADB_DATABASE"
-mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
-mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
-
-echo "make our changes take effect"
-mysql -e "FLUSH PRIVILEGES"
-
-service mysql stop
-
-mysqld_safe
+	service mysql stop
+fi
