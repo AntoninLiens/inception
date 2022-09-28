6a9bf0607778f4b686b35477a6b7a34ef0b51065
AntoninLiens <antoninliens@hotmail.be>
2022-09-28 15:18:26 +0200

    test

--- wordpress/utils/entrypoint.sh
+++ wordpress/utils/entrypoint.sh
@@ -5,28 +5,10 @@
 #                                                     +:+ +:+         +:+      #
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/09/26 17:44:18 by aliens            #+#    #+#              #
-#    Updated: 2022/09/28 14:34:22 by aliens           ###   ########.fr        #
+#    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
+#    Updated: 2022/09/28 14:43:33 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
-	service mysql start
-
-	echo "update root password"
-	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
-
-	echo "create $MARIADB_DATABASE"
-	mysql -e "create database $MARIADB_DATABASE"
-	mysql -e "create user '$MARIADB_USER'@'%' identified by '$MARIADB_USER_PASSWORD'"
-	mysql -e "grant all privileges on $MARIADB_DATABASE.* to $MARIADB_USER@'%'"
-
-	echo "make our changes take effect"
-	mysql -e "FLUSH PRIVILEGES"
-
-	service mysql stop
-fi
-
-mysqld_safe --datadir=/var/lib/mysql
