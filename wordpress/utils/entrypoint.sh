4980475fa518500c754b65cc3497b8aa1fd54ace
Antoninliens <antoninliens@hotmail.be>
2022-10-01 15:43:14 +0200

    maj

--- wordpress/utils/entrypoint.sh
+++ wordpress/utils/entrypoint.sh
@@ -6,9 +6,19 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
-#    Updated: 2022/09/28 14:43:33 by aliens           ###   ########.fr        #
+#    Updated: 2022/09/30 14:53:17 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
+rm -rf /var/www/worpress/wp-config.php
+
+wp config create \
+	--dbname=$MARIADB_DATABASE \
+	--dbuser=$MARIADB_USER \
+	--dbpass=$MARIADB_USER_PASSWORD \
+	--dbhost=$MARIADB_HOST \
+	--path="/var/www/wordpress/" \
+	--allow-root \
+	--skip-check
