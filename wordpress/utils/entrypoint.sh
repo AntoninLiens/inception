117393d56a90c4564d785f4ca9f5f30f0b8da3f7
Antoninliens <antoninliens@hotmail.be>
2022-10-01 17:18:17 +0200

    maj

--- wordpress/utils/entrypoint.sh
+++ wordpress/utils/entrypoint.sh
@@ -6,23 +6,11 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
-#    Updated: 2022/10/01 17:15:50 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/01 17:18:08 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
-
-rm -rf /var/www/worpress/wp-config.php
-
-echo "create config.php"
-wp config create \
-	--dbname=$MARIADB_DATABASE \
-	--dbuser=$MARIADB_USER \
-	--dbpass=$MARIADB_USER_PASSWORD \
-	--dbhost=$MARIADB_HOST \
-	--path="/var/www/wordpress/" \
-	--allow-root \
-	--skip-check
 
 if ! wp core is-installed --allow-root; then
 	echo "install wordpress"
@@ -59,5 +47,17 @@ if ! wp core is-installed --allow-root; then
 			ils arrivent pour vous manger !!!"
 		--allow-root
 fi
+
+rm -rf /var/www/worpress/wp-config.php
+
+echo "create config.php"
+wp config create \
+	--dbname=$MARIADB_DATABASE \
+	--dbuser=$MARIADB_USER \
+	--dbpass=$MARIADB_USER_PASSWORD \
+	--dbhost=$MARIADB_HOST \
+	--path="/var/www/wordpress/" \
+	--allow-root \
+	--skip-check
 
 php-fpm7.3 --nodemonize
