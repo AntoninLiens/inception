d3f9b58b338551ac4725104e72cad0de5e1eaed9
AntoninLiens <antoninliens@hotmail.be>
2022-10-06 11:20:30 +0200

    la passe

--- wordpress/utils/entrypoint.sh
+++ wordpress/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
-#    Updated: 2022/10/06 11:19:15 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/06 11:20:28 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,7 +24,7 @@ wp config create \
 	--allow-root \
 	--skip-check
 
-if ! wp core is-installed --allow-root; then
+if ! wp core is-installed --path="/var/www/wordpress/" --allow-root; then
 	echo "install wordpress"
 	wp core install \
 		--url=$WORDPRESS_URL \
