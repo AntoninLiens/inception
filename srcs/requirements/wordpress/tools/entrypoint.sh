706711c84ce8fe01ae11b6f78cc515cb16ff6ea1
AntoninLiens <antoninliens@hotmail.be>
2022-10-07 15:35:55 +0200

    la passe

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,31 +6,31 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
-#    Updated: 2022/10/07 15:17:22 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/07 15:35:48 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 echo "create config.php"
-rm -rf /var/www/wordpress/wp-config.php
+rm -rf /var/www/html/wordpress/wp-config.php
 wp config create \
 	--dbname=$MARIADB_DATABASE \
 	--dbuser=$MARIADB_USER \
 	--dbpass=$MARIADB_USER_PASSWORD \
 	--dbhost=$MARIADB_HOST \
-	--path="/var/www/wordpress/" \
+	--path="/var/www/html/wordpress/" \
 	--allow-root \
 	--skip-check
 
-if ! wp core is-installed --path="/var/www/wordpress/" --allow-root; then
+if ! wp core is-installed --path="/var/www/html/wordpress/" --allow-root; then
 	echo "install wordpress"
 	wp core install \
 		--url=$WORDPRESS_URL \
 		--title=$WORDPRESS_TITLE \
 		--admin_user=$WORDPRESS_ROOT \
 		--admin_password=$WORDPRESS_ROOT_PASSWORD \
-		--path="/var/www/wordpress/" \
+		--path="/var/www/html/wordpress/" \
 		--allow-root \
 		--skip-email
 fi
