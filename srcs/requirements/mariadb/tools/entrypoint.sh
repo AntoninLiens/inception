cdc43836c46a2120deaf1ee555279fcefd807146
AntoninLiens <antoninliens@hotmail.be>
2022-10-07 15:42:58 +0200

    la passe

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/26 17:44:18 by aliens            #+#    #+#              #
-#    Updated: 2022/10/07 15:30:39 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/07 15:42:45 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,11 +20,11 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	touch /var/run/mysqld/mysqlf.pid
 	
 
-	echo "update root password"
-	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
+	# echo "update root password"
+	# mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root'"
 	
-	echo "delete root access"
-	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+	# echo "delete root access"
+	# mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 
 	echo "create $MARIADB_DATABASE"
 	mysql -e "create database $MARIADB_DATABASE"
