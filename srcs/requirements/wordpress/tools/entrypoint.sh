452359da28fb238bf62df8b8c33726af36984ac8
AntoninLiens <antoninliens@hotmail.be>
2022-10-10 12:47:15 +0200

    la passe

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,48 +1,41 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    entrypoint.sh                                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/09/28 14:34:37 by aliens            #+#    #+#              #
-#    Updated: 2022/10/10 11:44:54 by aliens           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# # The www.conf file is needed for communication with the server
+# grep -E "listen = 9000" "/etc/php/7.3/fpm/pool.d/www.conf" > /dev/null 2>&1
 
-#!/bin/sh
+# # If not found it's useless to modify it 
+# if [ $? -ne 0 ]; then
+#  	echo "--Modifying configuration file"
+# 	# Replacing the listen part
+# 	 sed -i "s|.*listen = /run/php/php7.3-fpm.sock.*|listen = 9000|g" "/etc/php/7.3/fpm/pool.d/www.conf" 
+# fi
 
-echo "create config.php"
 rm -rf /var/www/html/wordpress/wp-config.php
 wp config create \
-	--dbname=$MARIADB_DATABASE \
-	--dbuser=$MARIADB_USER \
-	--dbpass=$MARIADB_USER_PASSWORD \
-	--dbhost=$MARIADB_HOST \
-	--path="/var/www/html/wordpress/" \
-	--allow-root \
-	--skip-check
-
-if ! wp core is-installed --path="/var/www/html/wordpress/" --allow-root; then
-	echo "install wordpress"
-	wp core install \
-		--url=$WORDPRESS_URL \
-		--title=$WORDPRESS_TITLE \
-		--admin_user=$WORDPRESS_ROOT \
-		--admin_password=$WORDPRESS_ROOT_PASSWORD \
+		--dbname=$MARIADB_DATABASE \
+		--dbuser=$MARIADB_USER \
+		--dbpass=$MARIADB_PASSWORD \
+		--dbhost=$MARIADB_HOST \
 		--path="/var/www/html/wordpress/" \
 		--allow-root \
-		--skip-email
+		--skip-check
+
+if ! wp core is-installed --allow-root; then
+	echo "install wordpress"
+	wp core install \
+		--url="$WORDPRESS_URL" \
+		--title="$WORDPRESS_TITLE" \
+		--admin_user="$WORDPRESS_ADMIN_USER" \
+		--admin_password="$WORDPRESS_ADMIN_PWD" \
+		--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+		--allow-root
 
 	echo "update wordpress"
 	wp plugin update --all --allow-root
 
 	echo "create first user"
-	wp user create $WORDPRESS_USER \
-		--user-pass=$WORDPRESS_USER_PASSWORD \
-		--role-editor \
-		--allow-root \
-		--skip-email
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+		--role=editor \
+		--user_pass=$WORDPRESS_USER_PWD \
+		--allow-root
 
 	echo "create first post"
 	wp post generate \
@@ -60,4 +53,4 @@ if ! wp core is-installed --path="/var/www/html/wordpress/" --allow-root; then
 		--allow-root
 fi
 
-php-fpm7.3 --nodemonize
+php-fpm7.3 --nodaemonize
