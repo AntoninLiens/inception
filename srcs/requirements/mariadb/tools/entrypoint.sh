4ae40489696671e49fec91145d1b85c86d49b625
AntoninLiens <antoninliens@hotmail.be>
2022-10-12 13:01:05 +0200

    la passe

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/10 11:54:04 by aliens            #+#    #+#              #
-#    Updated: 2022/10/10 17:27:52 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/12 13:00:51 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,5 +21,5 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	service mysql stop --datadir=/var/lib/mysql
 fi
 
-echo "start"
 mysqld_safe --datadir=/var/lib/mysql
+echo "start"
