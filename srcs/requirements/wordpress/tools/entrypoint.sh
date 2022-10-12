8bc3192be87ed97509d28cd66e28701e743d6700
AntoninLiens <antoninliens@hotmail.be>
2022-10-12 13:58:51 +0200

    la passe

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/10 14:08:22 by aliens            #+#    #+#              #
-#    Updated: 2022/10/12 13:46:12 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/12 13:58:48 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -45,16 +45,17 @@ if ! wp core is-installed --allow-root; then
 	echo "create first post"
 	wp post generate \
 		--count=1 \
-		--post_title= \
-			"Les Aliens sont partout,
-			ils sont dans les campagnes,
-			ils sont dans les villes !!!!" \
+		--post_title=$WORDPRESS_TITLE\
 		--post_author=$WORDPRESS_ADMIN_USER \
 		--post_content= \
-			"J'ai traversé les armées des
+			"Les Aliens sont partout,
+			ils sont dans les campagnes,
+			ils sont dans les villes !!!!
+			J'ai traversé les armées des
 			Aliens pour vous faire parvenir
-			ce message; Vous devez fuir, ils sont partout,
-			ils arrivent pour vous manger !!!" \
+			ce message; Vous devez fuir, ils
+			sont partout, ils arrivent pour 
+			vous manger !!!" \
 		--allow-root
 fi
 
