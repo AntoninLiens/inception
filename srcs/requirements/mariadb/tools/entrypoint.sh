8bb1adfba8a30ad91a36a849139997e332f09f44
AntoninLiens <antoninliens@hotmail.be>
2022-10-14 13:13:50 +0200

    la passe

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: aliens <aliens@student.s19.be>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/10 11:54:04 by aliens            #+#    #+#              #
-#    Updated: 2022/10/14 12:17:41 by aliens           ###   ########.fr        #
+#    Updated: 2022/10/14 13:13:41 by aliens           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,9 @@ if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
 	service mysql start --datadir=/var/lib/mysql
 
 	echo "create $MARIADB_DATABASE"
-	eval "echo \"$(cat config.sql)\"" | mariadb -u root -p $MARIADB_ROOT_PASSWORD
+	eval "echo \"$(cat config.sql)\"" | mariadb -u root
+
+	mysqladmin -u root password $MARIADB_ROOT_PASSWORD
 
 	service mysql stop --datadir=/var/lib/mysql
 fi
