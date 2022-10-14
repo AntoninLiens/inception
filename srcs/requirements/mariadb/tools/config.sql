26be3a19f6e25e689eddb8249082a36274bfd37e
AntoninLiens <antoninliens@hotmail.be>
2022-10-14 11:46:49 +0200

    la passe

--- srcs/requirements/mariadb/tools/config.sql
+++ srcs/requirements/mariadb/tools/config.sql
@@ -1,6 +1,6 @@
 UPDATE mysql.user SET Password = PASSWORD('$MARIADB_ROOT_PASSWORD') WHERE User = 'root';
 
-ALTER USER root@localhost IDENTIFIED VIA mysql_native_password;
+UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';
 
 CREATE DATABASE $MARIADB_DATABASE;
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASSWORD';
